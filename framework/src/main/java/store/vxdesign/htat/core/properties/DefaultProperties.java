c808c2d4cce2b66391beebdaed21457cbcf99cad
Roman Mashenkin <xromash@vxdesign.store>
2018-04-06 00:23:54 +0300

    Fix bug of properties classpaths
    Issue: #8
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

--- framework/src/main/java/store/vxdesign/htat/core/properties/DefaultProperties.java
+++ framework/src/main/java/store/vxdesign/htat/core/properties/DefaultProperties.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:default.properties")
+@PropertySource("classpath:./default.properties")
 class DefaultProperties {
 }
